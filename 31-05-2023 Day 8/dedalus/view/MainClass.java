8755e3574000dbab608396c79abbbc331cc9fcd0
Dilli <sadhasivadilli01@gmail.com>
2023-05-31 22:39:52 +0530

    day8 commmit

---
+++ 31-05-2023 Day 8/dedalus/view/MainClass.java
@@ -0,0 +1,104 @@
+package com.dedalus.view;
+
+import com.dedalus.controller.*;
+import com.dedalus.exception.UserNotFoundException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.function.BiPredicate;
+
+class ThreadA implements Runnable {
+
+    public void run() {
+
+        try {
+            Thread.sleep(7000);
+            System.out.println("The data is loading...");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to EMS :)");
+
+        try {
+            String username;
+            String password;
+
+            ThreadA df = new ThreadA();
+            Thread f = new Thread(df);
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            f.start();
+            System.out.println("Enter Username");
+            username = br.readLine();
+            System.out.println("Enter Password");
+            password = br.readLine();
+
+            BiPredicate<String, String> res = (un, pwd) -> un.equals("Dilli")&&pwd.equals("pass");
+
+            if (res.test(username, password)) {
+                System.out.println("Welcome " + username);
+                Employeecontroller ec = new Employeecontroller();
+                Scanner sc = new Scanner(System.in);
+                String ch;
+
+                do {
+                    System.out.println("Enter your choice");
+                    System.out.println("1. Add Employee");
+                    System.out.println("2. View Employee");
+                    System.out.println("3. Serialize to file");
+                    System.out.println("4. Deserialize from file");
+                    System.out.println("5. update the Employee");
+                    System.out.println("6. delete the Employee");
+
+                    int choice = sc.nextInt();
+                    switch (choice) {
+                        case 1:
+                            ec.addEmployee();
+                            break;
+                        case 2:
+                            ec.viewEmployee();
+                            break;
+                        case 3:
+                            ec.serialize(ec.getEmplist());
+                            break;
+                        case 4:
+                        	ec.deserialize("dedalus.txt");
+                            break;
+                        case 5:
+                        	ec.updateEmployee();
+                        	break;
+                        case 6:
+                        	ec.deleteEmployee();
+                            break;
+                        default:
+                            System.out.println("Enter a valid number");
+                            break;
+                    }
+
+                    System.out.println("Do you want to continue? (Y/y)");
+                    ch = sc.next();
+                } while (ch.equalsIgnoreCase("Y"));
+
+                System.out.println("Thanks for using!");
+                System.exit(0);
+
+            } else {
+                throw new UserNotFoundException();
+            }
+        } catch (UserNotFoundException unf) {
+            unf.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println("IO Exception: " + ioe.getMessage());
+        } finally {
+            System.out.println("Finally...");
+        }
+    }
+}
